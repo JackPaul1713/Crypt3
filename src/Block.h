77ba48123388850c37f25b75eb7c2f9568a10b59
JackPaul1713 <JackPaul1713@gmail.com>
2021-04-26 23:34:14 -0500

    Finished Block(except swap) finished block's functions tested block's functions

--- src/Block.h
+++ src/Block.h
@@ -1,37 +1,40 @@
 #ifndef BLOCK_H
 #define BLOCK_H
 
+#include <vector>
+
 class Block
 {
   private:
     // attributes
-    const std::vector<std::vector<char*>> block; // data
-    const int size; // x and y size
+    std::vector<std::vector<char*>> block; // data
+    int size; // x and y size
+    int length;
     bool full;
-    // helpers:
-    std::vector<char> getRow(int y);
-    std::vector<char> getCol(int x);
-    void setRow(int y, const std::vector<char> row);
-    void setCol(int x, const std::vector<char> col);
-    void swapRows(int y0, int y1);
-    void swapCols(int x0, int x1);
   public:
     // constructors:
-    Block() {} // default
+    Block(): block(), size(0), length(0), full(false) {} // default
     Block(char* bytes, int length, int pos, int size); // full
     Block(const Block& block); // copy
     // deconstructor:
     ~Block() {}
     // accessors:
-    std::vector<std::vector<char*>> getBlock() {return(block);}
-    int getSize() {return(size);}
-    bool isFull() {return(full);}
+    std::vector<std::vector<char*>> getBlock() const {return(block);}
+    int getSize() const {return(size);}
+    int getLength() const {return(length);}
+    bool isFull() const {return(full);}
+    std::vector<char> getRow(int y) const;
+    std::vector<char> getCol(int x) const;
     // mutators:
-    void load(char* bytes, int length, int size);
+    void load(char* bytes, int length, int pos, int size);
+    void setRow(int y, const std::vector<char> row);
+    void setCol(int x, const std::vector<char> col);
+    void swapRows(int y0, int y1);
+    void swapCols(int x0, int x1);
     // helpers:
     friend void swap(Block& block0, Block& block1);
     // overloads:
-    Block& operator=(Block block) {Block::swap(*this, block); return(*this);} // assignment
+    Block& operator=(Block block) {swap(*this, block); return(*this);} // assignment
 };
 
 #endif
