f2b9d968b20a637a426b52339706f48cab77dcf8
JackPaul1713 <JackPaul1713@gmail.com>
2021-05-06 22:34:59 -0500

    Redid Key Declaration, Finished Key Definition (except save fuctions) key objects now have 3 keys, a boolean key for length encryption, a character key for value encryption, and an integer key for position encryption. for save functions I plan on having a preamble to tell where the each key starts and ends in the file, however I did not do them because they need the cypher object to work

--- src/Key.cpp
+++ src/Key.cpp
@@ -2,25 +2,104 @@
 #include <vector>
 #include "Cypher.h"
 
+//// constructors ////
+Key(int length): // generate
+lenKey_(), valKey_(), locKey_()
+{
+  lenKey_ = generateLenKey(length);
+  valKey_ = generateValKey(length);
+  locKey_ = generateLocKey(length);
+}
+Key(char* characters, int length): // c-string custom
+lenKey_(), valKey_(), locKey_()
+{
+  // make keys:
+  lenKey_ = vector<bool>(length);
+  valKey_ = vector<char>(length);
+  locKey_ = vector<int>(length);
+  // set values:
+  for(int i = 0; i < length; i++) // loop through c-string
+  {
+    lenKey_.at(i) = (bool)(characters[i] % 2); // turn into bool
+    valKey_.at(i) = characters[i];
+    locKey_.at(i) = (int)characters[i]; // turn into int
+  }
+}
+Key(string str): // string custom
+lenKey_(), valKey_(), locKey_()
+{
+  // make keys:
+  lenKey_ = vector<bool>(str.length());
+  valKey_ = vector<char>(str.length());
+  locKey_ = vector<int>(str.length());
+  // set values:
+  for(int i = 0; i < str.length(); i++) // loop through string
+  {
+    lenKey_.at(i) = (bool)(str[i] % 2); // turn into bool
+    valKey_.at(i) = str[i];
+    locKey_.at(i) = (int)str[i]; // turn int int
+  }
+}
+Key(const Key& key): // copy
+lenKey_(key.lenKey_), valKey_(key.valKey_), locKey_(key.locKey_)
+{}
+
 //// save ////
-void upload(string filename, string password);
-void download(string filename, string password);
+void upload(string filename, string password) // TODO
+{
+
+}
+void download(string filename, string password) // TODO
+{
+
+}
 
 // mutators:
-void changeLength(int newLength);
-void regenerate();
+void changeLength(int newLength, bool len, bool val, bool loc)
+{
+  if(len) lenKey_ = generateLenKey(newLength);
+  if(val) valKey_ = generateLenKey(newLength);
+  if(loc) locKey_ = generateLenKey(newLength);
+}
+void regenerate(bool len, bool val, bool loc)
+{
+  if(len) lenKey_ = generateLenKey(lenKey_.size());
+  if(val) valKey_ = generateLenKey(valKey_.size());
+  if(loc) locKey_ = generateLenKey(locKey_.size());
+}
 
 //// helpers ////
 friend void swap(Key& key0, Key& key1)
 {
   // swap all attributes between objects:
-  swap(key0.key, key1.key);
+  swap(key0.lenKey_, key1.lenKey_);
+  swap(key0.valKey_, key1.valKey_);
+  swap(key0.locKey_, key1.locKey_);
 }
-char generateRandomByte(int number, int modifier)
+std::vector<bool> generateLenKey(int length) // generates length key
 {
-
+  vector<bool> lenKey(length);
+  for(int i = 0; i < length; i++) // loop through key
+  {
+    lenKey.at(i) = generateRandomBool(); // set to random boolean
+  }
+  return(lenKey);
 }
-std::vector<char> generateKey(int length) // generates key of lenght length(parameter)
+std::vector<char> generateValKey(int length) // generates value key
 {
-
+  vector<bool> valKey(length);
+  for(int i = 0; i < length; i++) // loop through key
+  {
+    valKey.at(i) = generateRandomChar(); // set to random character
+  }
+  return(valKey);
+}
+std::vector<int> generateLocKey(int length) // generates location key
+{
+  vector<bool> locKey(length);
+  for(int i = 0; i < length; i++) // loop through key
+  {
+    locKey.at(i) = generateRandomInt(); // set to random integer
+  }
+  return(locKey);
 }
--- src/Key.h
+++ src/Key.h
@@ -9,15 +9,22 @@ class Key
 {
   private:
     // attributes:
-    std::vector<char> key;
+    std::vector<bool> lenKey_;
+    std::vector<char> valKey_;
+    std::vector<int> locKey_;
     // helpers:
-    char generateRandomByte(int number, int modifier);
-    std::vector<char> generateKey(); // generates key of lenght length(attribute)
+    char generateRandomBool() {return(rand() % 2);}
+    char generateRandomByte() {return((rand() % 128) - 128);}
+    char generateRandomInt() {return(rand());}
+    std::vector<bool> generateLenKey(int length); // generates length key
+    std::vector<char> generateValKey(int length); // generates value key
+    std::vector<int> generateLocKey(int length); // generates location key
   public:
     // constructors:
     Key() {} // default
     Key(int length); // generate
-    Key(char* characters); // custom
+    Key(char* characters, int length); // c-string custom
+    Key(string str); // string custom
     Key(const Key& key); // copy
     // deconstructor:
     ~Key();
@@ -25,10 +32,12 @@ class Key
     void upload(string filename, string password);
     void download(string filename, string password);
     // accessors:
-    std::vector<char> getKey() {return(key);}
+    std::vector<bool> lenKey() {return(lenKey_);}
+    std::vector<char> valKey() {return(valKey_);}
+    std::vector<int> locKey() {return(locKey_);}
     // mutators:
-    void changeLength(int newLength);
-    void regenerate();
+    void changeLength(int newLength, bool len = true, bool val = true, bool loc = true);
+    void regenerate(bool len = true, bool val = true, bool loc = true);
     // helpers:
     friend void swap(Key& key0, Key& key1);
     // overloads:
